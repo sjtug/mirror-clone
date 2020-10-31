1a26b8180b1f5e645a272ba2f3ac11fc8b306d54
Alex Chi <iskyzh@gmail.com>
2020-10-31 21:04:51 +0800

    opam: support download from source site

--- overlay/src/lib.rs
+++ overlay/src/lib.rs
@@ -1,16 +1,36 @@
 use futures::future::{BoxFuture, FutureExt};
+use futures::lock::Mutex;
 use rand::distributions::Alphanumeric;
 use rand::prelude::*;
+use slog_scope::debug;
+use std::ffi::OsStr;
 use std::ffi::OsString;
 use std::io;
 use std::iter;
 use std::mem;
 use std::path::{Path, PathBuf};
+use std::sync::Arc;
+
+use std::collections::HashMap;
 use tokio::fs::{self, File, OpenOptions};
+
+type FusedFiles = Arc<Mutex<HashMap<PathBuf, bool>>>;
+
+#[derive(Clone, Debug)]
+pub struct OverlayFSError(String);
+
+impl std::fmt::Display for OverlayFSError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl std::error::Error for OverlayFSError {}
 
 pub struct OverlayDirectory {
     pub base_path: PathBuf,
     pub run_id: OsString,
+    fused_files: FusedFiles,
 }
 
 impl OverlayDirectory {
@@ -27,9 +47,10 @@ impl OverlayDirectory {
         let mut dir = Self {
             base_path,
             run_id: OsString::from(run_id),
+            fused_files: Arc::new(Mutex::new(HashMap::new())),
         };
 
-        dir.clean().await?;
+        dir.fuse_and_clean().await?;
 
         Ok(dir)
     }
@@ -40,20 +61,27 @@ impl OverlayDirectory {
         Ok(())
     }
 
-    async fn clean(&mut self) -> Result<(), io::Error> {
-        Self::clean_dir(self.base_path.clone()).await?;
+    async fn fuse_and_clean(&mut self) -> Result<(), io::Error> {
+        Self::fuse_and_clean_dir(self.base_path.clone(), self.fused_files.clone()).await?;
         Ok(())
     }
 
-    fn clean_dir(path: PathBuf) -> BoxFuture<'static, Result<(), io::Error>> {
+    fn fuse_and_clean_dir(
+        path: PathBuf,
+        fused_files: FusedFiles,
+    ) -> BoxFuture<'static, Result<(), io::Error>> {
         async move {
             let mut iter = fs::read_dir(path).await?;
             while let Some(entry) = iter.next_entry().await? {
                 let meta = fs::metadata(entry.path()).await?;
                 if meta.is_dir() {
-                    Self::clean_dir(entry.path()).await?;
+                    Self::fuse_and_clean_dir(entry.path(), fused_files.clone()).await?;
                 }
                 if meta.is_file() {
+                    {
+                        let mut fused_files = fused_files.lock().await;
+                        fused_files.insert(entry.path(), false);
+                    }
                     if let Some(path) = entry.file_name().to_str() {
                         if path.ends_with(".tmp") {
                             fs::remove_file(entry.path()).await?;
@@ -66,9 +94,23 @@ impl OverlayDirectory {
         .boxed()
     }
 
-    pub async fn add_to_overlay<P: AsRef<Path>>(&self, path: P) -> Result<bool, io::Error> {
+    fn check_within<P1: AsRef<Path>, P2: AsRef<Path>>(_base: P1, _check: P2) -> bool {
+        unimplemented!()
+    }
+
+    pub async fn try_fuse<P: AsRef<Path>>(&self, path: P) -> Result<bool, OverlayFSError> {
         let path = self.base_path.join(path);
-        Ok(path.exists())
+        let mut fused_files = self.fused_files.lock().await;
+        if let Some(x) = fused_files.get_mut(&path) {
+            if *x {
+                Err(OverlayFSError(format!("{:?} already fused", path)))
+            } else {
+                *x = true;
+                Ok(true)
+            }
+        } else {
+            Ok(false)
+        }
     }
 
     pub async fn create_file_for_write<P: AsRef<Path>>(
@@ -77,7 +119,7 @@ impl OverlayDirectory {
     ) -> Result<OverlayFile, io::Error> {
         let path = self.base_path.join(path);
         Self::create_folder_for_file(&path).await?;
-        OverlayFile::create_for_write(path, self.run_id.clone()).await
+        OverlayFile::create_for_write(path, self.run_id.clone(), self.fused_files.clone()).await
     }
 
     pub async fn create<P: AsRef<Path>>(
@@ -87,7 +129,16 @@ impl OverlayDirectory {
     ) -> Result<OverlayFile, io::Error> {
         let path = self.base_path.join(path);
         Self::create_folder_for_file(&path).await?;
-        OverlayFile::create(path, self.run_id.clone(), options).await
+        OverlayFile::create(path, self.run_id.clone(), options, self.fused_files.clone()).await
+    }
+
+    pub async fn commit(&self) -> Result<(), io::Error> {
+        for (path, fused) in self.fused_files.lock().await.iter() {
+            if !fused {
+                fs::remove_file(path).await?;
+            }
+        }
+        Ok(())
     }
 }
 
@@ -96,6 +147,7 @@ pub struct OverlayFile {
     pub run_id: OsString,
     pub path: PathBuf,
     pub file: Option<File>,
+    fuse_to: FusedFiles,
 }
 
 const TMP_FILE_SUFFIX: &str = ".tmp";
@@ -104,16 +156,18 @@ impl OverlayFile {
     pub async fn create_for_write<P: AsRef<Path>>(
         path: P,
         run_id: OsString,
+        fuse_to: FusedFiles,
     ) -> Result<Self, io::Error> {
         let mut options = OpenOptions::new();
         options.write(true).read(true);
-        Self::create(path, run_id, options).await
+        Self::create(path, run_id, options, fuse_to).await
     }
 
-    pub async fn create<P: AsRef<Path>>(
+    async fn create<P: AsRef<Path>>(
         path: P,
         run_id: OsString,
         mut options: OpenOptions,
+        fuse_to: FusedFiles,
     ) -> Result<Self, io::Error> {
         let path = path.as_ref().to_path_buf();
         options.create_new(true).truncate(true);
@@ -132,10 +186,14 @@ impl OverlayFile {
             file: Some(file),
             tmp_path,
             run_id,
+            fuse_to,
         })
     }
 
     pub async fn commit(mut self) -> Result<(), io::Error> {
+        let mut fuse_to = self.fuse_to.lock().await;
+        fuse_to.insert(self.path.clone(), true);
+        drop(fuse_to);
         mem::drop(self.file.take().unwrap());
         fs::rename(self.tmp_path.clone(), self.path.clone()).await?;
         Ok(())
@@ -267,5 +325,26 @@ mod tests {
         file.commit().await.unwrap();
         let file_path = tmp_dir.path().join("test/233/2333/233333.zip");
         assert!(file_path.exists());
+    }
+
+    #[test]
+    #[ignore]
+    fn test_within() {
+        for base in &[PathBuf::from("opam_test"), PathBuf::from("opam_test/")] {
+            assert!(!OverlayDirectory::check_within(base, "opam_test_2333"));
+            assert!(!OverlayDirectory::check_within(base, "opam_te"));
+            assert!(!OverlayDirectory::check_within(base, ""));
+            assert!(OverlayDirectory::check_within(base, "opam_test"));
+            assert!(OverlayDirectory::check_within(base, "opam_test/23333"));
+            assert!(OverlayDirectory::check_within(base, "opam_test/23333/"));
+            assert!(!OverlayDirectory::check_within(
+                base,
+                PathBuf::from("opam_test/2333333").join("..").join("..")
+            ));
+            assert!(!OverlayDirectory::check_within(
+                base,
+                PathBuf::from("opam_test/2333333").join("/opam_test")
+            ));
+        }
     }
 }
