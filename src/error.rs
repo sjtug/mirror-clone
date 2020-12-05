5f1b4522c3e1b5903e976315cdef627b79517e05
Alex Chi <iskyzh@gmail.com>
2020-12-05 21:16:41 +0800

    feat: initial commit of v2

--- src/error.rs
+++ src/error.rs
@@ -1,34 +1,11 @@
+use std::result;
+
 use thiserror::Error;
 
-#[derive(Error, Debug)]
+#[derive(Debug, Error)]
 pub enum Error {
-    #[error("IO Error {0}")]
-    Io(#[from] std::io::Error),
     #[error("Reqwest Error {0}")]
     Reqwest(#[from] reqwest::Error),
-    #[error("UTF8 Error {0}")]
-    UTF8Error(#[from] std::str::Utf8Error),
-    #[error("Regex Error {0}")]
-    RegexError(#[from] regex::Error),
-    #[error("Mock Error {0}")]
-    #[allow(unused)]
-    MockError(String),
-    #[error("Checksum Error {checksum_type} expected {expected}, get {checksum}")]
-    ChecksumError {
-        checksum_type: String,
-        expected: String,
-        checksum: String,
-    },
-    #[error("Length Mismatch expected {expected}, get {size}")]
-    LengthMismatch { expected: usize, size: usize },
-    #[error("HTTP Error {0}")]
-    HTTPError(reqwest::StatusCode),
-    #[error("JSON Decode Error {0}")]
-    JsonDecodeError(#[from] serde_json::Error),
-    #[error("Overlay Filesystem Error {0}")]
-    OverlayFSError(#[from] overlay::OverlayFSError),
-    #[error("WalkDir Error {0}")]
-    WalkDirError(#[from] walkdir::Error),
 }
 
-pub type Result<T> = std::result::Result<T, Error>;
+pub type Result<T> = result::Result<T, Error>;
--- src/main.rs
+++ src/main.rs
@@ -1,122 +1,76 @@
-#![feature(box_patterns)]
-mod conda;
-mod crates_io;
 mod error;
-mod opam;
-mod oracle;
-mod tar;
-mod utils;
 
-use clap::clap_app;
-use slog::{o, Drain, Level, LevelFilter};
-use slog_scope_futures::FutureExt;
-use std::path::PathBuf;
+use async_trait::async_trait;
 
-use crate::error::Result;
+use error::Result;
 
-fn create_logger(level: &str) -> slog::Logger {
-    // TODO: warn when using debug while compiled with release
-    let level = match level {
-        "info" => Level::Info,
-        "trace" => Level::Trace,
-        "warning" => Level::Warning,
-        "debug" => Level::Debug,
-        _ => panic!("unsupported log level"),
-    };
-    let decorator = slog_term::TermDecorator::new().build();
-    let drain = slog_term::FullFormat::new(decorator).build().fuse();
-    let drain = slog_async::Async::new(drain).chan_size(10240).build();
-    let drain = LevelFilter::new(drain, level).fuse();
-    slog::Logger::root(drain, o!())
+#[async_trait]
+pub trait SnapshotStorage<SnapshotItem> {
+    async fn snapshot(&mut self) -> Result<Vec<SnapshotItem>>;
+}
+
+#[async_trait]
+pub trait SourceStorage<SourceItem> {
+    async fn get_object(&self) -> Result<SourceItem>;
+}
+
+#[async_trait]
+pub trait TargetStorage<TargetItem> {
+    async fn put_object(&self, item: TargetItem) -> Result<()>;
+}
+
+pub struct Pypi;
+
+#[async_trait]
+impl SnapshotStorage<String> for Pypi {
+    async fn snapshot(&mut self) -> Result<Vec<String>> {
+        Ok(vec![])
+    }
+}
+
+#[async_trait]
+impl SourceStorage<String> for Pypi {
+    async fn get_object(&self) -> Result<String> {
+        Ok("".to_string())
+    }
+}
+
+pub struct S3Backend;
+
+#[async_trait]
+impl SnapshotStorage<String> for S3Backend {
+    async fn snapshot(&mut self) -> Result<Vec<String>> {
+        Ok(vec![])
+    }
+}
+
+#[async_trait]
+impl TargetStorage<String> for S3Backend {
+    async fn put_object(&self, item: String) -> Result<()> {
+        Ok(())
+    }
+}
+
+pub struct SimpleDiffTransfer<From, To>
+where
+    From: SourceStorage<String> + SnapshotStorage<String>,
+    To: TargetStorage<String> + SnapshotStorage<String>,
+{
+    from: From,
+    to: To,
+}
+
+impl<From, To> SimpleDiffTransfer<From, To>
+where
+    From: SourceStorage<String> + SnapshotStorage<String>,
+    To: TargetStorage<String> + SnapshotStorage<String>,
+{
+    async fn transfer() -> Result<()> {
+        Ok(())
+    }
 }
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let matches = clap_app!(mirror_clone =>
-        (version: "1.0")
-        (author: "Alex Chi <iskyzh@gmail.com>")
-        (about: "An all-in-one mirror utility by SJTUG")
-        (@arg progress: --progress ... "enable progress bar")
-        (@arg debug: --debug ... "enable debug mode")
-        (@arg loglevel: --log +takes_value default_value("info") "set log level")
-        (@subcommand opam =>
-            (about: "mirror OPAM repository")
-            (version: "1.0")
-            (author: "Alex Chi <iskyzh@gmail.com>")
-            (@arg repo: +required "OPAM repository")
-            (@arg dir: +required "clone directory")
-            (@arg archive: --archive +takes_value "OPAM archive URL")
-        )
-        (@subcommand conda =>
-            (about: "mirror Conda repository")
-            (version: "1.0")
-            (author: "Alex Chi <iskyzh@gmail.com>")
-            (@arg repo: +required "Conda repository")
-            (@arg dir: +required "clone directory")
-        )
-        (@subcommand crates_io =>
-            (about: "mirror crates.io repository")
-            (version: "1.0")
-            (author: "Alex Chi <iskyzh@gmail.com>")
-            (@arg repo: +required "crates.io index path")
-            (@arg dir: +required "clone directory")
-            (@arg src: +required "crates.io URL")
-        )
-    )
-    .get_matches();
-
-    let _guard =
-        slog_scope::set_global_logger(create_logger(matches.value_of("loglevel").unwrap()));
-
-    let oracle = oracle::Oracle {
-        client: reqwest::Client::new(),
-        progress: if matches.is_present("progress") {
-            indicatif::ProgressBar::new(1)
-        } else {
-            indicatif::ProgressBar::hidden()
-        },
-    };
-
-    match matches.subcommand() {
-        ("opam", Some(opam_matches)) => {
-            let repo = opam_matches.value_of("repo").unwrap().to_string();
-            opam::Opam {
-                base_path: PathBuf::from(opam_matches.value_of("dir").unwrap()),
-                repo: repo.clone(),
-                archive_url: opam_matches.value_of("archive").map(|x| x.to_string()),
-                debug_mode: matches.is_present("debug"),
-                concurrent_downloads: 16,
-            }
-            .run(oracle)
-            .with_logger(&slog_scope::logger().new(o!("task" => "opam", "repo" => repo)))
-            .await?;
-        }
-        ("conda", Some(conda_matches)) => {
-            let repo = conda_matches.value_of("repo").unwrap().to_string();
-            conda::Conda {
-                base_path: PathBuf::from(conda_matches.value_of("dir").unwrap()),
-                repo: repo.clone(),
-                debug_mode: matches.is_present("debug"),
-                concurrent_downloads: 16,
-            }
-            .run(oracle)
-            .with_logger(&slog_scope::logger().new(o!("task" => "conda", "repo" => repo)))
-            .await?;
-        }
-        ("crates_io", Some(crates_io_matches)) => {
-            crates_io::CratesIo {
-                base_path: PathBuf::from(crates_io_matches.value_of("dir").unwrap()),
-                repo_path: PathBuf::from(crates_io_matches.value_of("repo").unwrap()),
-                crates_io_url: crates_io_matches.value_of("src").unwrap().to_string(),
-                debug_mode: matches.is_present("debug"),
-                concurrent_downloads: 16,
-            }
-            .run(oracle)
-            .with_logger(&slog_scope::logger().new(o!("task" => "crates.io")))
-            .await?;
-        }
-        _ => {}
-    }
-
-    Ok(())
+async fn main() {
+    println!("Hello, World!");
 }
