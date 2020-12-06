f4e676eeb4131deced4c688221ee949cd213e824
Alex Chi <iskyzh@gmail.com>
2020-12-06 20:13:13 +0800

    fast resolve

--- src/rustup.rs
+++ src/rustup.rs
@@ -25,35 +25,63 @@ impl SnapshotStorage<String> for Rustup {
         let progress = mission.progress;
         let client = mission.client;
 
-        let mut snapshot = vec![];
         let channels = ["beta", "stable", "nightly"];
 
         info!(logger, "fetching channels...");
 
         let matcher = Regex::new(r#"url = "(.*)""#).unwrap();
 
+        let mut targets = vec![];
         for day_back in 1..self.days_to_retain {
             let now = Utc::now();
             let day = day_earlier(now, day_back as i64).unwrap();
             let day_string = day.format("%Y-%m-%d");
             for channel in &channels {
-                let target = format!("dist/{}/channel-rust-{}.toml", day_string, channel);
-                progress.set_message(&target);
-                let data = client
-                    .get(&format!("{}/{}", self.base, target))
-                    .send()
-                    .await?
-                    .text()
-                    .await?;
-
-                for capture in matcher.captures_iter(&data) {
-                    let url = &capture[1];
-                    let url = url.replace("https://static.rust-lang.org/", "");
-                    snapshot.push(url);
-                }
-                progress.inc(1);
+                targets.push((day_string.to_string(), channel.to_string()));
             }
         }
+
+        let packages: Result<Vec<Vec<String>>> =
+            futures::stream::iter(targets.into_iter().map(|(day_string, channel)| {
+                let client = client.clone();
+                let base = self.base.clone();
+                let progress = progress.clone();
+                let matcher = matcher.clone();
+                let logger = logger.clone();
+                let func = async move {
+                    let mut caps = vec![];
+                    let target = format!("dist/{}/channel-rust-{}.toml", day_string, channel);
+                    progress.set_message(&target);
+                    let data = client
+                        .get(&format!("{}/{}", base, target))
+                        .send()
+                        .await?
+                        .text()
+                        .await?;
+
+                    for capture in matcher.captures_iter(&data) {
+                        let url = &capture[1];
+                        let url = url.replace("https://static.rust-lang.org/", "");
+                        caps.push(url);
+                    }
+                    progress.inc(1);
+                    Ok::<Vec<String>, Error>(caps)
+                };
+                async move {
+                    match func.await {
+                        Ok(x) => Ok(x),
+                        Err(err) => {
+                            warn!(logger, "failed to fetch index {:?}", err);
+                            Ok(vec![])
+                        }
+                    }
+                }
+            }))
+            .buffer_unordered(256)
+            .try_collect()
+            .await;
+
+        let snapshot = packages?.into_iter().flatten().collect();
 
         progress.finish_with_message("done");
 
