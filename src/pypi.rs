19d4bc387b7cf2639b34fee1c2c8dff206553db1
Alex Chi <iskyzh@gmail.com>
2021-05-04 15:23:49 +0800

    pypi: fix missing packages

--- src/pypi.rs
+++ src/pypi.rs
@@ -126,7 +126,7 @@ impl SnapshotStorage<SnapshotPath> for Pypi {
         let snapshot = packages?
             .into_iter()
             .flatten()
-            .filter(|(url, _)| url.contains(".whl"))
+            .filter(|(url, _)| url.contains(".whl") || url.contains(".tar.gz"))
             .map(|(url, _)| {
                 if url.starts_with(&package_base) {
                     url[package_base.len()..].to_string()
