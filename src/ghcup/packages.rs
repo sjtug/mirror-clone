b077fd4789a237c52ce7200894553ecafd548755
LightQuantum <self@lightquantum.me>
2021-10-30 16:31:59 +0800

    fix(ghcup): migrate to github config repo (#105)

--- src/ghcup/packages.rs
+++ src/ghcup/packages.rs
@@ -3,11 +3,12 @@ use slog::{info, warn};
 
 use crate::common::{Mission, SnapshotConfig, TransferURL};
 use crate::error::{Error, Result};
+use crate::ghcup::utils::get_raw_blob_url;
 use crate::metadata::SnapshotMeta;
 use crate::traits::{SnapshotStorage, SourceStorage};
 
 use super::parser::{GhcupYamlParser, EXPECTED_CONFIG_VERSION};
-use super::utils::{fetch_last_tag, filter_map_file_objs, list_files};
+use super::utils::{filter_map_file_objs, list_files};
 use super::GhcupRepoConfig;
 
 #[derive(Debug, Clone)]
@@ -31,31 +32,28 @@ impl SnapshotStorage<SnapshotMeta> for GhcupPackages {
         info!(logger, "fetching ghcup config...");
         progress.set_message("querying version files");
         let latest_yaml_obj = filter_map_file_objs(
-            list_files(
-                &client,
-                repo_config,
-                fetch_last_tag(&client, repo_config).await?,
-            )
-            .await?,
+            list_files(&client, repo_config, &self.ghcup_repo_config.branch).await?,
         )
-        .max_by(|x, y| x.version().cmp(&y.version()))
+        .max_by(|x, y| x.version.cmp(&y.version))
         .ok_or_else(|| Error::ProcessError(String::from("no config file found")))?;
 
-        if latest_yaml_obj.version() != EXPECTED_CONFIG_VERSION {
+        if latest_yaml_obj.version != EXPECTED_CONFIG_VERSION {
             warn!(
                 logger,
                 "unmatched ghcup config yaml. expected: {}, got: {}",
                 EXPECTED_CONFIG_VERSION,
-                latest_yaml_obj.version()
+                latest_yaml_obj.version
             )
         }
 
         progress.set_message("downloading yaml config");
-        let yaml_url = format!(
-            "https://www.haskell.org/ghcup/data/{}",
-            latest_yaml_obj.name()
-        );
-        let yaml_data = client.get(&yaml_url).send().await?.bytes().await?;
+        let latest_yaml_blob_url = get_raw_blob_url(&client, repo_config, latest_yaml_obj).await?;
+        let yaml_data = client
+            .get(latest_yaml_blob_url.url)
+            .send()
+            .await?
+            .bytes()
+            .await?;
         let ghcup_config: GhcupYamlParser = serde_yaml::from_slice(&yaml_data)?;
 
         let fetch_uris: Vec<_> = ghcup_config
