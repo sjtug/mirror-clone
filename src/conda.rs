fdd458c96b3b25363ece5f005022dc0fe7143c13
LightQuantum <self@lightquantum.me>
2022-06-26 09:55:25 +0800

    conda: reduce memory usage (#110)

--- src/conda.rs
+++ src/conda.rs
@@ -5,17 +5,21 @@
 //! To ensure consistency, repository data is always transferred
 //! at the end. This is done by setting priority in snapshot metadata.
 
+use std::io;
+use std::io::ErrorKind;
+
+use async_trait::async_trait;
+use futures_util::{stream, StreamExt, TryStreamExt};
+use serde::de::DeserializeSeed;
+use serde::Deserialize;
+use slog::{info, warn};
+use structopt::StructOpt;
+use tokio_util::io::{StreamReader, SyncIoBridge};
+
 use crate::common::{Mission, SnapshotConfig, TransferURL};
 use crate::error::{Error, Result};
 use crate::metadata::SnapshotMeta;
 use crate::traits::{SnapshotStorage, SourceStorage};
-
-use async_trait::async_trait;
-use futures_util::{stream, StreamExt, TryStreamExt};
-use serde::Deserialize;
-use serde_json::Value as JsonValue;
-use slog::{info, warn};
-use structopt::StructOpt;
 
 #[derive(Debug, Clone, StructOpt)]
 pub struct CondaConfig {
@@ -35,27 +39,159 @@ pub struct Conda {
     repos: CondaRepos,
 }
 
-fn parse_index(repo: &str, data: &[u8]) -> Result<Vec<SnapshotMeta>> {
-    let v: JsonValue = serde_json::from_slice(data)?;
-    let mut result = vec![];
+mod de {
+    use std::fmt::Formatter;
 
-    let package_mapper = |(key, value): (&String, &JsonValue)| SnapshotMeta {
-        key: format!("{}/{}", repo, key),
-        size: value.get("size").map(|x| x.as_u64().unwrap()),
-        last_modified: None,
-        checksum_method: value.get("sha256").map(|_| "sha256".to_string()),
-        checksum: value.get("sha256").map(|x| x.as_str().unwrap().to_owned()),
-        ..Default::default()
-    };
+    use serde::de::{DeserializeSeed, IgnoredAny, MapAccess, Visitor};
+    use serde::Deserializer;
 
-    if let Some(JsonValue::Object(map)) = v.get("packages") {
-        result.append(&mut map.iter().map(package_mapper).collect());
-    }
-    if let Some(JsonValue::Object(map)) = v.get("packages.conda") {
-        result.append(&mut map.iter().map(package_mapper).collect());
+    use crate::metadata::SnapshotMeta;
+
+    pub struct Snapshot<'a> {
+        pub(crate) repo: &'a str,
     }
 
-    Ok(result)
+    impl<'de> DeserializeSeed<'de> for Snapshot<'de> {
+        type Value = Vec<SnapshotMeta>;
+
+        fn deserialize<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            struct MetadataVisitor<'a> {
+                repo: &'a str,
+            }
+
+            impl<'de> Visitor<'de> for MetadataVisitor<'de> {
+                type Value = Vec<SnapshotMeta>;
+
+                fn expecting(&self, formatter: &mut Formatter) -> std::fmt::Result {
+                    formatter.write_str("a map of conda metadata")
+                }
+
+                fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+                where
+                    A: MapAccess<'de>,
+                {
+                    let mut packages = vec![];
+
+                    while let Some(key) = map.next_key::<String>()? {
+                        if key == "packages" || key == "packages.conda" {
+                            packages
+                                .append(&mut map.next_value_seed(Packages { repo: self.repo })?);
+                        } else {
+                            map.next_value::<IgnoredAny>()?;
+                        }
+                    }
+
+                    Ok(packages)
+                }
+            }
+
+            deserializer.deserialize_map(MetadataVisitor { repo: self.repo })
+        }
+    }
+
+    struct Packages<'a> {
+        repo: &'a str,
+    }
+
+    impl<'de> DeserializeSeed<'de> for Packages<'de> {
+        type Value = Vec<SnapshotMeta>;
+
+        fn deserialize<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            struct PackagesVisitor<'a> {
+                repo: &'a str,
+            }
+
+            impl<'de> Visitor<'de> for PackagesVisitor<'de> {
+                type Value = Vec<SnapshotMeta>;
+
+                fn expecting(&self, formatter: &mut Formatter) -> std::fmt::Result {
+                    formatter.write_str("a map of conda packages")
+                }
+
+                fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+                where
+                    A: MapAccess<'de>,
+                {
+                    let mut packages = vec![];
+
+                    while let Some(key) = map.next_key::<String>()? {
+                        packages.push(map.next_value_seed(Package {
+                            repo: self.repo,
+                            name: key,
+                        })?);
+                    }
+
+                    Ok(packages)
+                }
+            }
+
+            deserializer.deserialize_map(PackagesVisitor { repo: self.repo })
+        }
+    }
+
+    struct Package<'a> {
+        repo: &'a str,
+        name: String,
+    }
+
+    impl<'de> DeserializeSeed<'de> for Package<'de> {
+        type Value = SnapshotMeta;
+
+        fn deserialize<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            struct PackageVisitor<'a> {
+                repo: &'a str,
+                name: String,
+            }
+
+            impl<'de> Visitor<'de> for PackageVisitor<'de> {
+                type Value = SnapshotMeta;
+
+                fn expecting(&self, formatter: &mut Formatter) -> std::fmt::Result {
+                    formatter.write_str("a map representing a single conda package")
+                }
+
+                fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+                where
+                    A: MapAccess<'de>,
+                {
+                    let mut size = None;
+                    let mut sha256 = None;
+                    while let Some(key) = map.next_key::<String>()? {
+                        if key == "size" {
+                            size = Some(map.next_value::<u64>()?);
+                        } else if key == "sha256" {
+                            sha256 = Some(map.next_value::<String>()?);
+                        } else {
+                            map.next_value::<IgnoredAny>()?;
+                        }
+                    }
+
+                    Ok(SnapshotMeta {
+                        key: format!("{}/{}", self.repo, self.name),
+                        size,
+                        last_modified: None,
+                        checksum_method: sha256.as_ref().map(|_| "sha256".to_string()),
+                        checksum: sha256,
+                        ..Default::default()
+                    })
+                }
+            }
+
+            deserializer.deserialize_map(PackageVisitor {
+                repo: self.repo,
+                name: self.name,
+            })
+        }
+    }
 }
 
 impl Conda {
@@ -94,9 +230,23 @@ impl SnapshotStorage<SnapshotMeta> for Conda {
             let future = async move {
                 let mut snapshot = vec![];
                 let repodata = format!("{}/{}/repodata.json", base, repo);
-                let index_data = client.get(&repodata).send().await?.bytes().await?;
-                let packages = parse_index(&repo, &index_data)?;
-                snapshot.extend(packages.into_iter());
+                let stream = client
+                    .get(&repodata)
+                    .send()
+                    .await?
+                    .bytes_stream()
+                    .map_err(|e| io::Error::new(ErrorKind::Other, e));
+                let reader = SyncIoBridge::new(StreamReader::new(stream));
+                let mut packages = {
+                    let repo = repo.clone();
+                    tokio::task::spawn_blocking(move || {
+                        let mut deserializer = serde_json::de::Deserializer::from_reader(reader);
+                        de::Snapshot { repo: &repo }.deserialize(&mut deserializer)
+                    })
+                    .await
+                    .expect("task panicked")?
+                };
+                snapshot.append(&mut packages);
                 progress.set_message(&repo);
                 snapshot.append(&mut vec![
                     SnapshotMeta::force(format!("{}/repodata.json", repo)),
