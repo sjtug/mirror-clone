1e12211c1bc643088bc4fa44ffa5d54c9b9c14b6
Alissa Tung <alissa-tung@outlook.com>
2023-10-19 01:19:51 +0800

    elan: nightly

--- src/main.rs
+++ src/main.rs
@@ -332,7 +332,10 @@ fn main() {
                     true,
                 );
                 let glean_src = stream_pipe::ByteStreamPipe::new(
-                    GitHubRelease::new(String::from("alissa-tung/glean"), 1),
+                    GitHubRelease::new(
+                        String::from("alissa-tung/glean"),
+                        source.retain_glean_versions,
+                    ),
                     buffer_path.clone().unwrap(),
                     true,
                 );
@@ -344,12 +347,22 @@ fn main() {
                     buffer_path.clone().unwrap(),
                     true,
                 );
+                let lean_nightly_src = stream_pipe::ByteStreamPipe::new(
+                    GitHubRelease::new(
+                        String::from("leanprover/lean4-nightly"),
+                        source.retain_lean_nightly_versions,
+                    ),
+                    buffer_path.clone().unwrap(),
+                    true,
+                );
                 let lean_org_repo_src = merge_pipe! {
                     lean4: lean_src,
+                    lean4_nightly: lean_nightly_src,
                 };
                 let unified = merge_pipe! {
                     elan: elan_src,
                     leanprover: lean_org_repo_src,
+                    glean: glean_src,
                 };
                 let indexed = index_pipe::IndexPipe::new(
                     unified,
